b6d86d3816d1fe9c885f33ea40e83bf1432874e9
Tino Fileccia <tinofileccia@yahoo.com>
2019-01-30 19:34:52 -0600

    fixed sizing issue and added parallax option for desktop

--- dist/kaleidoBackground.js
+++ dist/kaleidoBackground.js
@@ -110,6 +110,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return KaleidoBackground; });
 /* harmony import */ var webvr_polyfill__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! webvr-polyfill */ "./node_modules/webvr-polyfill/build/webvr-polyfill.js");
 /* harmony import */ var webvr_polyfill__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(webvr_polyfill__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var rellax__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! rellax */ "./node_modules/rellax/rellax.js");
+/* harmony import */ var rellax__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(rellax__WEBPACK_IMPORTED_MODULE_1__);
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -125,18 +127,20 @@ Sensitivity
 */
 
 
+
 var KaleidoBackground =
 /*#__PURE__*/
 function () {
   _createClass(KaleidoBackground, [{
     key: "drawImageProp",
-    value: function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+    value: function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY, scale) {
       if (arguments.length === 2) {
         x = y = 0;
         w = ctx.canvas.width;
         h = ctx.canvas.height;
-      } // default offset is center
+      }
 
+      scale = typeof scale === "number" ? scale : 1; // default offset is center
 
       offsetX = typeof offsetX === "number" ? offsetX : 0.5;
       offsetY = typeof offsetY === "number" ? offsetY : 0.5; // keep bounds [0.0, 1.0]
@@ -216,6 +220,27 @@ function () {
       }.bind(this));
     }
   }, {
+    key: "enableParallax",
+    value: function enableParallax(target) {
+      var parallaxItem = new rellax__WEBPACK_IMPORTED_MODULE_1___default.a(".".concat(this.canvasClass), {});
+    }
+  }, {
+    key: "resize",
+    value: function resize(e) {
+      this.targets.forEach(function (targetEl, i) {
+        var boundingRect = targetEl.getBoundingClientRect();
+        var height = boundingRect.height;
+        var width = boundingRect.width; //console.log(`width: ${width}, height: ${height}`);
+
+        this.ctxArray[i].height = height;
+        this.ctxArray[i].width = width;
+        this.containerArray[i].style.height = height;
+        this.containerArray[i].style.width = width;
+        this.containerArray[i].children[0].height = height;
+        this.containerArray[i].children[0].width = width;
+      }.bind(this));
+    }
+  }, {
     key: "animate",
     value: function animate() {
       requestAnimationFrame(this.animate);
@@ -224,33 +249,39 @@ function () {
       var x = (this.originalOrientation[1] - orientation[1]) * this.sensitivity * this.scaleConstant;
       var y = (this.originalOrientation[0] - orientation[0]) * this.sensitivity * this.scaleConstant;
       this.ctxArray.forEach(function (el) {
-        this.drawImageProp(el.ctx, this.image, -x - el.xOff, -y - el.yOff, el.width, el.height);
+        this.drawImageProp(el.ctx, this.image, x, y, el.width, el.height);
       }.bind(this));
     }
   }]);
 
   function KaleidoBackground(target, imageSource) {
     var _ref = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {
-      sensitivity: 1
+      sensitivity: 1,
+      parallax: false
     },
         _ref$sensitivity = _ref.sensitivity,
-        sensitivity = _ref$sensitivity === void 0 ? 1 : _ref$sensitivity;
+        sensitivity = _ref$sensitivity === void 0 ? 0.5 : _ref$sensitivity,
+        _ref$parallax = _ref.parallax,
+        parallax = _ref$parallax === void 0 ? false : _ref$parallax;
 
     _classCallCheck(this, KaleidoBackground);
 
     if (!target) {
-      throw new Error('No Kaleido target was specified.');
+      throw new Error('No target was specified.');
     }
 
     this.sensitivity = sensitivity;
     this.scaleConstant = 200;
     this.originalOrientation = [0, 0, 0];
+    this.resize = this.resize.bind(this);
     this.enableAccelerometer = this.enableAccelerometer.bind(this);
     this.animate = this.animate.bind(this);
+    this.enableParallax = this.enableParallax.bind(this);
     this.enableAccelerometer();
     this.image = document.createElement('img');
     this.image.setAttribute('src', imageSource);
     this.ctxArray = [];
+    this.containerArray = [];
 
     window.onload = function () {
       this.targets = document.querySelectorAll(target);
@@ -265,12 +296,24 @@ function () {
         var height = boundingRect.height;
         var width = boundingRect.width; //Create and size canvas
 
-        var canvas = document.createElement('canvas');
+        var canvas = document.createElement('canvas'); //Create unique class name (used for Rellax.js)
+
+        this.canvasClass = Math.floor(Math.random() * 100000) + 1;
+        this.canvasClass = 'gyroCanvas-' + this.canvasClass;
+        canvas.classList.add(this.canvasClass);
         canvas.width = width;
         canvas.height = height;
-        canvas.style.position = 'absolute';
-        canvas.style.zIndex = -1;
-        var ctx = canvas.getContext('2d'); //Draw Image if the source is valid
+        var ctx = canvas.getContext('2d'); //Create div container for canvas so that 3D CSS scaling can be taken advantage of
+
+        var container = document.createElement('div');
+        container.className = 'gyro-container';
+        container.style.position = 'absolute';
+        container.style.zIndex = -1;
+        container.style.overflow = 'hidden';
+        container.width = width;
+        container.height = height;
+        container.append(canvas);
+        this.containerArray.push(container); //Draw Image if the source is valid
 
         try {
           this.drawImageProp(ctx, this.image, 0, 0, width, height);
@@ -278,39 +321,369 @@ function () {
           throw new Error('Unable to draw selected Image, please check the image source.');
         }
 
-        var scalingSize = this.scaleConstant * this.sensitivity * 2 * 2;
-        var newWidth, newHeight;
-
-        if (height > width) {
-          newHeight = height + scalingSize;
-          newWidth = newHeight / height * width;
-        } else {
-          newWidth = width + scalingSize;
-          newHeight = newWidth / width * height;
-        }
-
         this.ctxArray.push({
           ctx: ctx,
-          width: newWidth,
-          height: newHeight,
-          xOff: (newWidth - width) / 2,
-          yOff: (newHeight - height) / 2
+          width: width,
+          height: height,
+          scale: 1 + this.sensitivity * 0.1
         });
-        targetEl.prepend(canvas);
+        targetEl.prepend(container);
       }.bind(this));
 
-      if (this.vrDisplay) {
+      if (!this.vrDisplay && parallax) {
+        this.enableParallax(target);
+      } else {
+        this.containerArray.forEach(function (container) {
+          container.children[0].style.transform = "scale(".concat(1 + this.sensitivity * 0.8, ")");
+        }.bind(this));
         this.vrDisplay.getFrameData(this.frameData);
         this.originalOrientation = [this.frameData.pose.orientation[0], this.frameData.pose.orientation[1], this.frameData.pose.orientation[2], this.frameData.pose.orientation[3]];
         this.animate();
       }
     }.bind(this);
+
+    window.addEventListener('resize', this.resize);
   }
 
   return KaleidoBackground;
 }();
 
 
+
+/***/ }),
+
+/***/ "./node_modules/rellax/rellax.js":
+/*!***************************************!*\
+  !*** ./node_modules/rellax/rellax.js ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(global) {var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;
+// ------------------------------------------
+// Rellax.js
+// Buttery smooth parallax library
+// Copyright (c) 2016 Moe Amaya (@moeamaya)
+// MIT license
+//
+// Thanks to Paraxify.js and Jaime Cabllero
+// for parallax concepts
+// ------------------------------------------
+
+(function (root, factory) {
+  if (true) {
+    // AMD. Register as an anonymous module.
+    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory),
+				__WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ?
+				(__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+  } else {}
+}(typeof window !== "undefined" ? window : global, function () {
+  var Rellax = function(el, options){
+    "use strict";
+
+    var self = Object.create(Rellax.prototype);
+
+    var posY = 0;
+    var screenY = 0;
+    var posX = 0;
+    var screenX = 0;
+    var blocks = [];
+    var pause = true;
+
+    // check what requestAnimationFrame to use, and if
+    // it's not supported, use the onscroll event
+    var loop = window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      function(callback){ return setTimeout(callback, 1000 / 60); };
+
+    // store the id for later use
+    var loopId = null;
+
+    // check what cancelAnimation method to use
+    var clearLoop = window.cancelAnimationFrame || window.mozCancelAnimationFrame || clearTimeout;
+
+    // check which transform property to use
+    var transformProp = window.transformProp || (function(){
+        var testEl = document.createElement('div');
+        if (testEl.style.transform === null) {
+          var vendors = ['Webkit', 'Moz', 'ms'];
+          for (var vendor in vendors) {
+            if (testEl.style[ vendors[vendor] + 'Transform' ] !== undefined) {
+              return vendors[vendor] + 'Transform';
+            }
+          }
+        }
+        return 'transform';
+      })();
+
+    // Default Settings
+    self.options = {
+      speed: -2,
+      center: false,
+      wrapper: null,
+      relativeToWrapper: false,
+      round: true,
+      vertical: true,
+      horizontal: false,
+      callback: function() {},
+    };
+
+    // User defined options (might have more in the future)
+    if (options){
+      Object.keys(options).forEach(function(key){
+        self.options[key] = options[key];
+      });
+    }
+
+    // By default, rellax class
+    if (!el) {
+      el = '.rellax';
+    }
+
+    // check if el is a className or a node
+    var elements = typeof el === 'string' ? document.querySelectorAll(el) : [el];
+
+    // Now query selector
+    if (elements.length > 0) {
+      self.elems = elements;
+    }
+
+    // The elements don't exist
+    else {
+      throw new Error("The elements you're trying to select don't exist.");
+    }
+
+    // Has a wrapper and it exists
+    if (self.options.wrapper) {
+      if (!self.options.wrapper.nodeType) {
+        var wrapper = document.querySelector(self.options.wrapper);
+
+        if (wrapper) {
+          self.options.wrapper = wrapper;
+        } else {
+          throw new Error("The wrapper you're trying to use don't exist.");
+        }
+      }
+    }
+
+
+    // Get and cache initial position of all elements
+    var cacheBlocks = function() {
+      for (var i = 0; i < self.elems.length; i++){
+        var block = createBlock(self.elems[i]);
+        blocks.push(block);
+      }
+    };
+
+
+    // Let's kick this script off
+    // Build array for cached element values
+    var init = function() {
+      for (var i = 0; i < blocks.length; i++){
+        self.elems[i].style.cssText = blocks[i].style;
+      }
+
+      blocks = [];
+
+      screenY = window.innerHeight;
+      screenX = window.innerWidth;
+      setPosition();
+
+      cacheBlocks();
+
+      // If paused, unpause and set listener for window resizing events
+      if (pause) {
+        window.addEventListener('resize', init);
+        pause = false;
+      }
+      animate();
+    };
+
+    // We want to cache the parallax blocks'
+    // values: base, top, height, speed
+    // el: is dom object, return: el cache values
+    var createBlock = function(el) {
+      var dataPercentage = el.getAttribute( 'data-rellax-percentage' );
+      var dataSpeed = el.getAttribute( 'data-rellax-speed' );
+      var dataZindex = el.getAttribute( 'data-rellax-zindex' ) || 0;
+
+      // initializing at scrollY = 0 (top of browser), scrollX = 0 (left of browser)
+      // ensures elements are positioned based on HTML layout.
+      //
+      // If the element has the percentage attribute, the posY and posX needs to be
+      // the current scroll position's value, so that the elements are still positioned based on HTML layout
+      var wrapperPosY = self.options.wrapper ? self.options.wrapper.scrollTop : (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop);
+      // If the option relativeToWrapper is true, use the wrappers offset to top, subtracted from the current page scroll.
+      if (self.options.relativeToWrapper) {
+        var scrollPosY = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop);
+        wrapperPosY = scrollPosY - self.options.wrapper.offsetTop;
+      }
+      var posY = self.options.vertical ? ( dataPercentage || self.options.center ? wrapperPosY : 0 ) : 0;
+      var posX = self.options.horizontal ? ( dataPercentage || self.options.center ? (window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft) : 0 ) : 0;
+
+      var blockTop = posY + el.getBoundingClientRect().top;
+      var blockHeight = el.clientHeight || el.offsetHeight || el.scrollHeight;
+
+      var blockLeft = posX + el.getBoundingClientRect().left;
+      var blockWidth = el.clientWidth || el.offsetWidth || el.scrollWidth;
+
+      // apparently parallax equation everyone uses
+      var percentageY = dataPercentage ? dataPercentage : (posY - blockTop + screenY) / (blockHeight + screenY);
+      var percentageX = dataPercentage ? dataPercentage : (posX - blockLeft + screenX) / (blockWidth + screenX);
+      if(self.options.center){ percentageX = 0.5; percentageY = 0.5; }
+
+      // Optional individual block speed as data attr, otherwise global speed
+      var speed = dataSpeed ? dataSpeed : self.options.speed;
+
+      var bases = updatePosition(percentageX, percentageY, speed);
+
+      // ~~Store non-translate3d transforms~~
+      // Store inline styles and extract transforms
+      var style = el.style.cssText;
+      var transform = '';
+
+      // Check if there's an inline styled transform
+      if (style.indexOf('transform') >= 0) {
+        // Get the index of the transform
+        var index = style.indexOf('transform');
+
+        // Trim the style to the transform point and get the following semi-colon index
+        var trimmedStyle = style.slice(index);
+        var delimiter = trimmedStyle.indexOf(';');
+
+        // Remove "transform" string and save the attribute
+        if (delimiter) {
+          transform = " " + trimmedStyle.slice(11, delimiter).replace(/\s/g,'');
+        } else {
+          transform = " " + trimmedStyle.slice(11).replace(/\s/g,'');
+        }
+      }
+
+      return {
+        baseX: bases.x,
+        baseY: bases.y,
+        top: blockTop,
+        left: blockLeft,
+        height: blockHeight,
+        width: blockWidth,
+        speed: speed,
+        style: style,
+        transform: transform,
+        zindex: dataZindex
+      };
+    };
+
+    // set scroll position (posY, posX)
+    // side effect method is not ideal, but okay for now
+    // returns true if the scroll changed, false if nothing happened
+    var setPosition = function() {
+      var oldY = posY;
+      var oldX = posX;
+
+      posY = self.options.wrapper ? self.options.wrapper.scrollTop : (document.documentElement || document.body.parentNode || document.body).scrollTop || window.pageYOffset;
+      posX = self.options.wrapper ? self.options.wrapper.scrollLeft : (document.documentElement || document.body.parentNode || document.body).scrollLeft || window.pageXOffset;
+      // If option relativeToWrapper is true, use relative wrapper value instead.
+      if (self.options.relativeToWrapper) {
+        var scrollPosY = (document.documentElement || document.body.parentNode || document.body).scrollTop || window.pageYOffset;
+        posY = scrollPosY - self.options.wrapper.offsetTop;
+      }
+
+
+      if (oldY != posY && self.options.vertical) {
+        // scroll changed, return true
+        return true;
+      }
+
+      if (oldX != posX && self.options.horizontal) {
+        // scroll changed, return true
+        return true;
+      }
+
+      // scroll did not change
+      return false;
+    };
+
+    // Ahh a pure function, gets new transform value
+    // based on scrollPosition and speed
+    // Allow for decimal pixel values
+    var updatePosition = function(percentageX, percentageY, speed) {
+      var result = {};
+      var valueX = (speed * (100 * (1 - percentageX)));
+      var valueY = (speed * (100 * (1 - percentageY)));
+
+      result.x = self.options.round ? Math.round(valueX) : Math.round(valueX * 100) / 100;
+      result.y = self.options.round ? Math.round(valueY) : Math.round(valueY * 100) / 100;
+
+      return result;
+    };
+
+    // Loop
+    var update = function() {
+      if (setPosition() && pause === false) {
+        animate();
+      }
+
+      // loop again
+      loopId = loop(update);
+    };
+
+    // Transform3d on parallax element
+    var animate = function() {
+      var positions;
+      for (var i = 0; i < self.elems.length; i++){
+        var percentageY = ((posY - blocks[i].top + screenY) / (blocks[i].height + screenY));
+        var percentageX = ((posX - blocks[i].left + screenX) / (blocks[i].width + screenX));
+
+        // Subtracting initialize value, so element stays in same spot as HTML
+        positions = updatePosition(percentageX, percentageY, blocks[i].speed);// - blocks[i].baseX;
+        var positionY = positions.y - blocks[i].baseY;
+        var positionX = positions.x - blocks[i].baseX;
+
+        var zindex = blocks[i].zindex;
+
+        // Move that element
+        // (Set the new translation and append initial inline transforms.)
+        var translate = 'translate3d(' + (self.options.horizontal ? positionX : '0') + 'px,' + (self.options.vertical ? positionY : '0') + 'px,' + zindex + 'px) ' + blocks[i].transform;
+        self.elems[i].style[transformProp] = translate;
+      }
+      self.options.callback(positions);
+    };
+
+    self.destroy = function() {
+      for (var i = 0; i < self.elems.length; i++){
+        self.elems[i].style.cssText = blocks[i].style;
+      }
+
+      // Remove resize event listener if not pause, and pause
+      if (!pause) {
+        window.removeEventListener('resize', init);
+        pause = true;
+      }
+
+      // Clear the animation loop to prevent possible memory leak
+      clearLoop(loopId);
+      loopId = null;
+    };
+
+    // Init
+    init();
+
+    // Start the loop
+    update();
+
+    // Allow to recalculate the initial values whenever we want
+    self.refresh = init;
+
+    return self;
+  };
+  return Rellax;
+}));
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../webpack/buildin/global.js */ "./node_modules/webpack/buildin/global.js")))
 
 /***/ }),
 
--- index.js
+++ index.js
@@ -7,18 +7,20 @@ Sensitivity
 */
 
 import WebVRPolyfill from 'webvr-polyfill';
-
+import Relax from 'rellax';
 
 
 export default class KaleidoBackground {
 
-  drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+  drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY, scale) {
 
     if (arguments.length === 2) {
         x = y = 0;
         w = ctx.canvas.width;
         h = ctx.canvas.height;
     }
+
+    scale = typeof scale === "number" ? scale : 1;
 
     // default offset is center
     offsetX = typeof offsetX === "number" ? offsetX : 0.5;
@@ -106,6 +108,37 @@ export default class KaleidoBackground {
 
   }
 
+  enableParallax( target ) {
+
+    let parallaxItem = new Relax( `.${this.canvasClass}`, {
+
+    });
+
+  }
+
+  resize( e ) {
+
+    this.targets.forEach( function( targetEl, i ) {
+
+      let boundingRect = targetEl.getBoundingClientRect();
+      let height = boundingRect.height;
+      let width = boundingRect.width
+
+      //console.log(`width: ${width}, height: ${height}`);
+
+      this.ctxArray[i].height = height;
+      this.ctxArray[i].width = width;
+
+      this.containerArray[i].style.height = height;
+      this.containerArray[i].style.width = width;
+
+      this.containerArray[i].children[0].height = height;
+      this.containerArray[i].children[0].width = width;
+
+    }.bind( this ));
+
+  }
+
   animate() {
 
     requestAnimationFrame( this.animate );
@@ -117,29 +150,32 @@ export default class KaleidoBackground {
 
     this.ctxArray.forEach(function( el ) {
 
-      this.drawImageProp( el.ctx, this.image, -x - el.xOff, -y - el.yOff, el.width, el.height )
+      this.drawImageProp( el.ctx, this.image, x, y, el.width, el.height )
 
     }.bind( this ));
 
   }
 
-  constructor( target, imageSource, { sensitivity = 1 } = { sensitivity: 1 } ) {
+  constructor( target, imageSource, { sensitivity = 0.5, parallax = false } = { sensitivity: 1, parallax: false } ) {
 
     if ( !target ) {
-      throw new Error('No Kaleido target was specified.');
+      throw new Error('No target was specified.');
     }
 
     this.sensitivity = sensitivity;
     this.scaleConstant = 200;
     this.originalOrientation = [0, 0, 0];
+    this.resize = this.resize.bind( this );
     this.enableAccelerometer = this.enableAccelerometer.bind( this );
     this.animate = this.animate.bind( this );
+    this.enableParallax = this.enableParallax.bind( this );
 
     this.enableAccelerometer();
 
     this.image = document.createElement('img');
     this.image.setAttribute('src', imageSource);
     this.ctxArray = [];
+    this.containerArray = []
 
     window.onload = function () {
 
@@ -158,11 +194,28 @@ export default class KaleidoBackground {
 
         //Create and size canvas
         let canvas = document.createElement('canvas');
+
+        //Create unique class name (used for Rellax.js)
+        this.canvasClass = Math.floor(Math.random() * 100000) + 1;
+        this.canvasClass = 'gyroCanvas-' + this.canvasClass;
+        canvas.classList.add( this.canvasClass );
+
         canvas.width = width;
         canvas.height = height;
-        canvas.style.position = 'absolute';
-        canvas.style.zIndex = -1;
         let ctx = canvas.getContext('2d');
+
+        //Create div container for canvas so that 3D CSS scaling can be taken advantage of
+
+        let container = document.createElement('div');
+        container.className = 'gyro-container';
+        container.style.position = 'absolute';
+        container.style.zIndex = -1;
+        container.style.overflow = 'hidden';
+        container.width = width;
+        container.height = height;
+
+        container.append( canvas );
+        this.containerArray.push( container );
 
         //Draw Image if the source is valid
         try {
@@ -171,39 +224,38 @@ export default class KaleidoBackground {
           throw new Error('Unable to draw selected Image, please check the image source.');
         }
 
-        let scalingSize = ( this.scaleConstant * this.sensitivity * 2 ) * 2;
-        let newWidth, newHeight;
-
-        if( height > width ) {
-
-          newHeight = height + scalingSize;
-          newWidth = ( newHeight / height ) * width;
-
-        } else {
-
-          newWidth = width + scalingSize;
-          newHeight = ( newWidth / width ) * height;
-
-        }
-
         this.ctxArray.push( {
           ctx: ctx,
-          width: newWidth,
-          height: newHeight,
-          xOff: (newWidth - width) / 2,
-          yOff: (newHeight - height) / 2
+          width: width,
+          height: height,
+          scale: 1 + ( this.sensitivity * 0.1 )
         } );
-        targetEl.prepend( canvas );
+
+        targetEl.prepend( container );
 
       }.bind( this ));
 
-      if ( this.vrDisplay ) {
+      if ( !this.vrDisplay && parallax ) {
+
+        this.enableParallax( target );
+
+      } else {
+
+        this.containerArray.forEach(function ( container ) {
+
+          container.children[0].style.transform = `scale(${ 1 + ( this.sensitivity * 0.8 ) })`;
+
+        }.bind( this ));
+
         this.vrDisplay.getFrameData( this.frameData );
         this.originalOrientation = [ this.frameData.pose.orientation[0], this.frameData.pose.orientation[1], this.frameData.pose.orientation[2], this.frameData.pose.orientation[3] ] ;
         this.animate();
+
       }
 
     }.bind( this );
+
+    window.addEventListener('resize', this.resize)
 
   }
 
